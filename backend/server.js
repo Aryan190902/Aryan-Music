c8e5d26a03b3ec50a9dcf89d69bbb323ae2b5ed7
Aryan apte <b20186@students.iitmandi.ac.in>
2024-12-12 18:33:42 +0530

    Updated server.js
    Positioned dotenv to the top, and removed the default PORT value.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const authRoutes = require('./routes/auth.js');
 const adminRoutes = require('./routes/admin.js');
 const imageRoutes = require('./routes/image.js');
